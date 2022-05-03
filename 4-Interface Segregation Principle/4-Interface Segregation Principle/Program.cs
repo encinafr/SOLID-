9186d560ca5d9bd9d9053344306eb014a47810ca
Javier <javier.encina@engee.com.ar>
2022-05-03 19:39:50 -0300

    Interface Segregation Principal

---
+++ 4-Interface Segregation Principle/4-Interface Segregation Principle/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace _4_Interface_Segregation_Principle
+{
+    public class Document
+    {
+
+    }
+
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+    }
+
+    public class MultiFunctionrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            //
+        }
+
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+
+        public class OldFashionedPrinter : IMachine
+        {
+            public void Fax(Document d)
+            {
+               //
+            }
+
+            public void Print(Document d)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Scan(Document d)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    public class Photocopier : IPrinter, IScanner
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+           //
+        }
+    }
+
+    public interface IMultiFuncionDevice : IPrinter, IScanner //....
+    {
+
+    }
+
+    public class MultifuncionMachine : IMultiFuncionDevice
+    {
+        private IPrinter printer;
+        private IScanner scanner;
+
+        public MultifuncionMachine(IPrinter printer, IScanner scanner)
+        {
+            this.printer = printer;
+            this.scanner = scanner;
+        }
+
+        public void Print(Document d)
+        {
+            printer.Print(d)
+        }
+
+        public void Scan(Document d)
+        {
+            scanner.Scan(d);
+        }//Decorator Pattern
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
